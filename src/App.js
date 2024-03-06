e8f6714eca4b8f4a17188d9b79e538e5c66e441d
Durdona Ibragimova <durdona.dev@gmail.com>
2024-03-06 14:47:42 -0500

    Create simple accordion component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [expand, setExpand] = useState(false);
+
+    const handleOnClick = () => {
+        setExpand(!expand);
+    };
+
+    return (
+        <div className="App">
+            <div className="accordion">
+                <div className="accordion__header" onClick={handleOnClick}>
+                    <h2>What is the concept of time management?</h2>
+                    <span className={expand ? "rotate" : ""}> &#10095;</span>
+                </div>
+                {expand ? (
+                    <div className="accordion__body">
+                        <p>
+                            Time management refers to the process of organizing
+                            and planning how to divide your time between
+                            specific activities effectively to increase
+                            productivity and achieve goals.
+                        </p>
+                    </div>
+                ) : null}
+            </div>
+        </div>
+    );
 }
-
-export default App;
